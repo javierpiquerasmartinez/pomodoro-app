784251c8892a7d1ee68c798b739983162b971db8
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2025-03-13 14:51:02 +0100

    Funcionalidad autostart

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const USER_BREAK_2 = 0.2;
   const STEPS = [USER_STUDY, USER_BREAK_1, USER_STUDY, USER_BREAK_1, USER_STUDY, USER_BREAK_1, USER_STUDY, USER_BREAK_2];
   
+  var auto_start = document.getElementById('auto-start').checked;
+  
   // Current time
   var time;
   // Current timer
@@ -12,8 +14,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
   var step;
 
   // Initialize the step
-  function setStep(reload) {
-    console.log('step1: ', step, reload);
+  function setStep({reload, autoStart}) {
+    console.log('step1: ', step, reload, autoStart);
     if (!reload) {
       step = (step === undefined) ? 0 : ++step;
     }
@@ -21,6 +23,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
     time = STEPS[step] * 60;
     document.getElementById('step').innerHTML = STEPS[step] === USER_STUDY ? 'Study' : 'Break';
     updateTimer();
+    if (autoStart && !reload) {
+      startTimer();
+    }
   }
 
   // Function that starts a countdown timer from 25 minutes to 0
@@ -33,7 +38,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
       updateTimer();
       time--;
       if (time < 0) {
-        reloadTimer(false);
+        reloadTimer(false, auto_start);
       }
     }, 1000);
   }
@@ -56,12 +61,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
   }
 
   // Reload the timer
-  function reloadTimer(reload) {
+  function reloadTimer(reload, autoStart) {
     clearInterval(timer);
     document.getElementById('pause-resume-btn').removeEventListener('click', handlePauseTimer);
     document.getElementById('pause-resume-btn').innerHTML = 'Start';
     document.getElementById('pause-resume-btn').addEventListener('click', handleStartTimer);
-    setStep(reload);
+    setStep({reload: reload, autoStart: autoStart});
   }
 
   function handleRestartTimer() {
@@ -77,12 +82,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
   }
 
   function handleNextStep() {
-    reloadTimer(false);
+    reloadTimer(false, false);
   }
 
+  // Initialize the buttons
   document.getElementById('restart-btn').addEventListener('click', handleRestartTimer);
   document.getElementById('forward-btn').addEventListener('click', handleNextStep);
+  document.getElementById('pause-resume-btn').addEventListener('click', handleStartTimer);
+  document.getElementById('auto-start').addEventListener('change', (event) => {
+    auto_start = event.target.checked;
+  });
 
-  setStep();
-  startTimer();
+  setStep({autoStart: false});
 });
